c591faf74d0d22b0796a5159da1316485daf00f6
Phan Thế Thịnh <145144191+Thinhpt264@users.noreply.github.com>
2023-10-13 10:53:49 +0700

    Update BreadthFirstSearchAlgo.java

--- TriTueNhanTao/src/k21/BreadthFirstSearchAlgo.java
+++ TriTueNhanTao/src/k21/BreadthFirstSearchAlgo.java
@@ -5,59 +5,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-public class BreadthFirstSearchAlgo implements ISearchAlgo {
-
+public class BreadthFirstSearchAlgo implements ISearchAlgo{
 	@Override
 	public Node execute(Node root, String goal) {
-		// TODO Auto-generated method stub	
-		Queue<Node> queue = new LinkedList<>();
-		Set<Node> visited = new HashSet<>();
-		
-		queue.add(root);
-		while(!queue.isEmpty()) {
-			Node current = queue.poll();
-			if(current.getLabel().equals(goal)) {
-				return current;
-			}
-			visited.add(current);
-			for (Edge edge : current.getChildren()) {
-				Node adjacent = edge.getEnd();
-				if (!visited.contains(adjacent)) {
-                    queue.add(adjacent);
-                }
+		// TODO Auto-generated method stub
+		if (root.getLabel().equals(goal)) return root;
+		Queue<Node> frontier = new LinkedList<Node>();
+		List<Node> explored = new ArrayList<>();
+		frontier.add(root);
+		while (!frontier.isEmpty()) {
+			Node currentNode = frontier.poll();
+			if(currentNode.getLabel().equals(goal)) return currentNode;
+			explored.add(currentNode);
+			List<Node> children = currentNode.getChildrenNodes();
+			for (Node child : children) {
+				if (!frontier.contains(child) && !explored.contains(child)) {
+					child.setParent(currentNode);
+					frontier.add(child);
+				}
 			}
 		}
 		return null;
 	}
 
 	@Override
-	 public Node execute(Node root, String start, String goal) {
-        Node startNode = findNode(root, start);
+	public Node execute(Node root, String start, String goal) {
+		// TODO Auto-generated method stub
+		if (root.getLabel().equals(goal)) return root;
+		if (start.equals(goal)) return new Node(goal);
+		Node strartNode = execute(root, start);
+		strartNode.setParent(null);
+		return execute(strartNode, goal);
+	}
 
-        if (startNode == null) {
-            return null; // Start node not found
-        }
-
-        return execute(startNode, goal);
-    }
-
-    private Node findNode(Node root, String data) {
-        if (root == null) {
-            return null;
-        }
-
-        if (root.getLabel().equals(data)) {
-            return root;
-        }
-
-        for (Edge edge : root.getChildren()) {
-            Node adjacent = edge.getEnd();
-            Node result = findNode(adjacent, data);
-            if (result != null) {
-                return result;
-            }
-        }
-
-        return null;
-    }
 }
